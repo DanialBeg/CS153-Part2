772ec432cd89e638ad8d35592c1394baa2e43e73
rsohi001 <rsohi001@ucr.edu>
2020-03-08 06:19:32 +0000

    Edits to shm_open, no testing done as of now

--- shm.c
+++ shm.c
@@ -30,14 +30,15 @@ void shminit() {
 
 int shm_open(int id, char **pointer) {
 int first_proc = 1;
+proc *curproc = myproc();
 acquire(&(shm_table.lock)); //mutual exclusion
  //CASE 1: Check the smh_table to see if the id that is trying to be opened already existed
 for(unsigned int i = 0; i < 64; i++){
 	if(shm_table.shm_pages[i].id == id){
-		mappages(curproc->pgdir, sz, PGSIZE, shm_table.shm_pages[i].frame, PTE_W|PTE_U);
+		mappages(curproc()->pgdir, PGROUNDUP(curporc()->sz), PGSIZE, shm_table.shm_pages[i].frame, PTE_W|PTE_U);
 		shm_table.shm_pages[i].refcnt++;
-		*pointer = (char *)sz;
-		sz++;
+		*pointer = (char *)PGROUNDUP(curproc()->sz);
+		curproc()->sz++;
 		first_proc = 0;
 	}
 }
@@ -47,12 +48,12 @@ for(unsigned int i = 0; i < 64; i++){
 if(first_proc == 1){
 	for(unsigned int i = 0; i < 64; i++){
 		if(shm_table.shm_pages[i].id == 0){
-			shm_table.shm_pages[i].id = id;
-			//kmalloc a page and store it into shm_table frame
-			shm_table.shm_pages[i].refcnt = 1;
-			mappages(curproc->pgdir, sz, PGSIZE, shm_table.shm_pages[i].frame, PTE_W|PTE_U);
-			*pointer = (char *)sz;
-			sz++;
+			shm_table.shm_pages[i].id = id; // The new id is this
+			shm_table.shm_pages[i].frame = kalloc(); 
+			shm_table.shm_pages[i].refcnt = 1; // Only one item is connected here
+			mappages(curproc->pgdir, PGROUNDUP(curproc()->sz), PGSIZE, shm_table.shm_pages[i].frame, PTE_W|PTE_U);
+			*pointer = (char *)PGROUNDUP(curproc()->sz);
+			curproc()->sz++;		
 			break;
 		}
 	}		
