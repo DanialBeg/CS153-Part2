470879834320100292334172d7532173c5a8b97f
Danial Beg <danny00b@gmail.com>
2020-03-08 20:18:39 +0000

    Fixed compilation issues

--- shm.c
+++ shm.c
@@ -31,6 +31,48 @@ void shminit() {
 int shm_open(int id, char **pointer) {
 
 //you write this
+//int first_proc = 1;
+//struct proc *curproc = myproc();
+
+acquire(&(shm_table.lock)); //mutual exclusion
+
+int first_proc = 1;
+struct proc *curproc = myproc();
+
+ //CASE 1: Check the smh_table to see if the id that is trying to be opened already existed
+for(unsigned int i = 0; i < 64; i++){
+	if(shm_table.shm_pages[i].id == id){
+		shm_table.shm_pages[i].refcnt++;
+		mappages(curproc->pgdir, (void *)PGROUNDUP(curproc->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+		//shm_table.shm_pages[i].refcnt++;
+		*pointer = (char *)PGROUNDUP(curproc->sz);
+		curproc->sz = PGROUNDUP(curproc->sz) + PGSIZE;
+		first_proc = 0;
+	}
+}
+//END CASE1
+
+//CASE 2: If the id is not on the shm_table we will make our id the first to do so
+if(first_proc == 1){
+	unsigned int i;
+	for(i = 0; i < 64; i++){
+		if(shm_table.shm_pages[i].id == 0 && shm_table.shm_pages[i].frame == 0 && shm_table.shm_pages[i].refcnt == 0){
+			//break;
+			shm_table.shm_pages[i].id = id; // The new id is this
+			char* memory = kalloc();
+			memset(memory, 0, PGSIZE);
+			shm_table.shm_pages[i].frame = memory; 
+			shm_table.shm_pages[i].refcnt += 1; // Only one item is connected here
+			mappages(curproc->pgdir, (void *)PGROUNDUP(curproc->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+			*pointer = (char *)PGROUNDUP(curproc->sz);
+			curproc->sz = PGROUNDUP(curproc->sz) + PGSIZE;		
+			break;
+		}
+	}
+}
+
+
+release(&(shm_table.lock)); // mutual exclusion ending
 
 
 
